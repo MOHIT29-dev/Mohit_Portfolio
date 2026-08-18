d5103f6aaa7b4484c71d3b399d0cdb5662d025e4
Mohit Kumar <mohitkumar291202@gmail.com>
2026-08-19 03:48:47 +0530

    Changes are done of paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/Mohit_Portfolio/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        base: env.GITHUB_PAGES === 'true'
+            ? '/Mohit_Portfolio/'
+            : '/',
+        plugins: [react()],
+    }
 })
